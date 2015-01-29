c72c55fada7851bcb5274293d89bfa57497a6dfa
Dan Pasanen <dan.pasanen@gmail.com>
2015-01-28 16:41:29 -0800

    fs_mgr: Skip filesystem check unless fs_type matches
    * Prevent accidentally destroying a partition of the wrong type.
    * Don't skip iterations of the internal mount_all loop, otherwise
      encryptability may not be properly handled.
    
    Change-Id: I8f3ddc396a5fb85f4ae0a0a11dd61fb4d6462d6d
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- fs_mgr/Android.mk
+++ fs_mgr/Android.mk
@@ -29,8 +29,9 @@ LOCAL_C_INCLUDES := \
     system/extras/ext4_utils \
     external/openssl/include \
     bootable/recovery \
+    system/extras/ext4_utils \
+    system/extras/squashfs_utils \
     external/e2fsprogs/lib
-
 LOCAL_MODULE:= libfs_mgr
 LOCAL_STATIC_LIBRARIES := $(common_static_libraries)
 LOCAL_C_INCLUDES += system/extras/ext4_utils system/extras/squashfs_utils external/e2fsprogs/lib
