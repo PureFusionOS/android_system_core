4f265858dc9655e211702d674a811337c0586930
Amit Pundir <amit.pundir@linaro.org>
2016-06-29 19:00:00 +0530

    init: select usb gadget controller at run time
    Right now we set sys.usb.controller property in init.$platform.usb.rc
    to enable ConfigFS gadgets.
    
    Let system detect and set UDC driver name from /sys/class/udc instead.
    
    Change-Id: I99ad49d24dc53b543c5573e209bf00d6c2d91dd4
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- init/init.cpp
+++ init/init.cpp
@@ -602,22 +602,24 @@ static void selinux_initialize(bool in_kernel_domain) {
     }
 }
 
-static int charging_mode_booting(void) {
-#ifndef BOARD_CHARGING_MODE_BOOTING_LPM
-    return 0;
-#else
-    int f;
-    char cmb;
-    f = open(BOARD_CHARGING_MODE_BOOTING_LPM, O_RDONLY);
-    if (f < 0)
-        return 0;
+// Set the UDC controller for the ConfigFS USB Gadgets.
+// Read the UDC controller in use from "/sys/class/udc".
+// In case of multiple UDC controllers select the first one.
+static void set_usb_controller() {
+    static const char sys_udc[] = "/sys/class/udc";
 
-    if (1 != read(f, (void *)&cmb,1))
-        return 0;
+    std::unique_ptr<DIR, int(*)(DIR*)>dir(opendir(sys_udc), closedir);
+    if (!dir) return;
 
-    close(f);
-    return ('1' == cmb);
-#endif
+    dirent *dp;
+    while ((dp = readdir(dir.get())) != NULL) {
+        if (dp->d_name[0] == '.') {
+            continue;
+        }
+
+        property_set("sys.usb.controller", dp->d_name);
+        break;
+    }
 }
 
 int main(int argc, char** argv) {
@@ -713,6 +715,7 @@ int main(int argc, char** argv) {
     property_load_boot_defaults();
     export_oem_lock_status();
     start_property_service();
+    set_usb_controller();
 
     const BuiltinFunctionMap function_map;
     Action::set_function_map(&function_map);
